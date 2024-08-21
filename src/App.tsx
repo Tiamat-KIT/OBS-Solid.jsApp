e9e0be0019c485f8addc2defe27805fb727d51f2
ｳﾀｶﾀｷｮｳｽｲ <c1101081@planet.kanazawa-it.ac.jp>
2024-08-21 18:03:36 +0000

    テスト公開してみる

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { createSignal } from 'solid-js'
+import solidLogo from './assets/solid.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = createSignal(0)
+
+  if(typeof window.obsstudio === "undefined"){
+    console.error("OBSのブラウザソース環境下で動作していません")
+  }
+
+  const obs = window.obsstudio
+
+  return (
+    <>
+      <p>return {obs.pluginVersion}</p>
+    </>
+  )
+}
+
+export default App
