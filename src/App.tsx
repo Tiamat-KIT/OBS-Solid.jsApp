1871da97cb92b41c9828baaa79cead48115857be
ｳﾀｶﾀｷｮｳｽｲ <c1101081@planet.kanazawa-it.ac.jp>
2024-08-21 18:08:20 +0000

    これもいらない

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from 'solid-js'
+/* import { createSignal } from 'solid-js' */
 /* import solidLogo from './assets/solid.svg'
 import viteLogo from '/vite.svg' */
 import './App.css'
