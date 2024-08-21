c187155defee57bbef12cb24af7b5a13d7de68eb
ｳﾀｶﾀｷｮｳｽｲ <c1101081@planet.kanazawa-it.ac.jp>
2024-08-21 18:54:23 +0000

    いったんローカルで作業

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 /* import { createSignal } from 'solid-js' */
 /* import solidLogo from './assets/solid.svg'
 import viteLogo from '/vite.svg' */
-import './App.css'
 import { ObsEventListener } from './ObsEventListener'
+import "./tailwind.css"
 
 function App() {
   /* const [count, setCount] = createSignal(0) */
 
   if(typeof window.obsstudio === "undefined"){
     console.error("OBSのブラウザソース環境下で動作していません")
+    return <p class='text-2xl text-center font-bold'>not OBS</p>
   }
 
   const obs = window.obsstudio
 
   return (
     <>
-      <p>return {obs.pluginVersion}</p>
+      <h3>return {obs.pluginVersion}</h3>
       <ObsEventListener obs={obs} />
     </>
   )
